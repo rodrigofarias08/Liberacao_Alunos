781677b56a233c00142c95e7e23426063eb84789
Rodrigo Farias <rodrigofarias08@gmail.com>
2019-04-18 21:02:41 -0300

    first commit

---
+++ src/main/java/br/senai/sc/LiberacaoAlunosApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberacaoAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiberacaoAlunosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/senai/sc/model/Administrador.java
@@ -0,0 +1,7 @@
+package br.senai.sc.model;
+
+public class Administrador {
+	
+	
+
+}
